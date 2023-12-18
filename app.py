2777d254f91414aa9da4172a28f7e7cbbb2b75e9
timha <46964476+timha@users.noreply.github.com>
2023-12-18 01:30:44 -0800

    add is_cat function

--- app.py
+++ app.py
@@ -2,9 +2,12 @@ import gradio as gr
 from fastai.vision.all import *
 import skimage
 
+
+def is_cat(x): return x[0].isupper()
 learn = load_learner('model.pkl')
 
 labels = learn.dls.vocab
+
 def predict(img):
     img = PILImage.create(img)
     pred,pred_idx,probs = learn.predict(img)
