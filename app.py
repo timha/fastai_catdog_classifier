f5912ac4c27ffe3cd489d65990078f59962b1701
timha <46964476+timha@users.noreply.github.com>
2023-12-18 01:15:37 -0800

    deploy to huggingface spaces

---
+++ app.py
@@ -0,0 +1,20 @@
+import gradio as gr
+from fastai.vision.all import *
+import skimage
+
+learn = load_learner('model.pkl')
+
+labels = learn.dls.vocab
+def predict(img):
+    img = PILImage.create(img)
+    pred,pred_idx,probs = learn.predict(img)
+    return {labels[i]: float(probs[i]) for i in range(len(labels))}
+
+title = "Pet Breed Classifier"
+description = "A pet breed classifier trained on the Oxford Pets dataset with fastai. Created as a demo for Gradio and HuggingFace Spaces."
+article="<p style='text-align: center'><a href='https://tmabraham.github.io/blog/gradio_hf_spaces_tutorial' target='_blank'>Blog post</a></p>"
+examples = ['grey-cat.jpeg']
+interpretation='default'
+enable_queue=True
+
+gr.Interface(fn=predict,inputs=gr.inputs.Image(shape=(512, 512)),outputs=gr.outputs.Label(num_top_classes=3),title=title,description=description,article=article,examples=examples,interpretation=interpretation,enable_queue=enable_queue).launch()
